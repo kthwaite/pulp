b1c7edb43482a13e96869aa1fb1fa824c998f584
cthwaite <28732700+cthwaite@users.noreply.github.com>
2020-07-31 14:09:28 +0100

    feat: Add error variant for serde_json

--- src/error.rs
+++ src/error.rs
@@ -3,6 +3,7 @@ pub enum Error {
     NoChapters,
     IoError(std::io::Error),
     RegexError(regex::Error),
+    JsonError(serde_json::Error),
 }
 
 impl std::error::Error for Error { }
@@ -12,6 +13,7 @@ impl std::fmt::Display for Error {
             Error::NoChapters => write!(f, "No chapters found!"),
             Error::RegexError(err) => write!(f, "{}", err),
             Error::IoError(err) => write!(f, "{}", err),
+            Error::JsonError(err) => write!(f, "{}", err),
         }
     }
 }
@@ -24,5 +26,10 @@ impl std::convert::From<std::io::Error> for Error {
 impl std::convert::From<regex::Error> for Error {
     fn from(err: regex::Error) -> Self {
         Error::RegexError(err)
+    }
+}
+impl std::convert::From<serde_json::Error> for Error {
+    fn from(err: serde_json::Error) -> Self {
+        Error::JsonError(err)
     }
 }
